b7e48b46d145109abbbc2175c1694e33231f0358
alex <san1295@tut.by>
2020-09-17 17:19:58 +0300

    fix

--- src/js/common.js
+++ src/js/common.js
@@ -108,7 +108,7 @@ jQuery(($) => {
 				smartSpeed: 700,
 				autoplay: true,
 				navText: ['<i class="fa fa-chevron-left"></i>', '<i class="fa fa-chevron-right"></i>'],
-				responsive: { 0: { items: 2 }, 768: { items: 3 }, 1024: { items: 4 } },
+				responsive: { 0: { items: 1 }, 768: { items: 2 }, 1024: { items: 4 } },
 			})
 
 
